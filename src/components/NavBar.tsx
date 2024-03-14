b57652de025f2db542fd52449d00deca1e208966
anuragpampati <anuragpampati696@gmail.com>
2024-03-13 22:46:11 -0400

    made icon and heading clickable

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Flex, HStack, Image, Link, Spacer, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -9,11 +9,22 @@ interface Props {
 
 const NavBar = ({ onSearch }: Props) => {
   return (
-    <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
-      <SearchInput onSearch={onSearch} />
-      <ColorModeSwitch />
-    </HStack>
+    <Flex padding="10px" alignItems="center">
+      <Link onClick={() => window.location.reload()}>
+        <Image src={logo} boxSize="60px" />
+      </Link>
+      <Link onClick={() => window.location.reload()}>
+        <Text fontSize="xl" fontWeight="bold" mr="auto">
+          Game Hub
+        </Text>
+      </Link>
+
+      <Spacer />
+      <HStack spacing="4">
+        <SearchInput onSearch={onSearch} />
+        <ColorModeSwitch />
+      </HStack>
+    </Flex>
   );
 };
 
